090677272c7521e48a9a9213029bd1c19b99b261
Damir Mikic <damir@pixupplay.com>
2025-11-05 11:50:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ const PLAYER_RADIUS = 13;
 const BALL_RADIUS = 8; 
 const GOAL_WIDTH = 10;
 const GOAL_HEIGHT = 100;
-const MAX_PLAYER_SPEED = 8.5;
+const MAX_PLAYER_SPEED = 9.5;
 const GOALKEEPER_SPEED = 2.5;
-const MAX_BALL_SPEED = 12;
+const MAX_BALL_SPEED = 13;
 
 // Field dimensions
 const PENALTY_BOX_WIDTH = 80; // 16-yard box width
@@ -3170,5 +3170,6 @@ function syncOverlayWithGameState() {
         updateOverlayBettingStatus('closed');
     }
 }
+
 
 
