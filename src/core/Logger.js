fb59feab4269d330afb22e85e226d84ae2373d58
Damir Mikic <damir@pixupplay.com>
2025-11-06 08:31:16 +0100

    Delete src directory

--- src/core/Logger.js
+++
@@ -1,5 +0,0 @@
-export const log = (...args) => {
-  if (process.env.NODE_ENV === 'development') {
-    console.log(...args); // eslint-disable-line no-console
-  }
-};
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import { initGame } from './core/gameManager.js';
-import { initUI } from './ui/UIInitializer.js';
-
-initGame();
-initUI();
